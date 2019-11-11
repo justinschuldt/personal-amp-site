2311ff4f1f31c7528312860b5055b77ec1361868
Justin Schuldt <justinschuldt@gmail.com>
2019-11-10 22:24:22 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal AMP site
+
+AMP compatible basic personal landing page.
+
+Uses Gulp to validate AMP markup, remove unused css, insert css inline, and minify.
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var purify = require('gulp-purifycss');
+var cleanCSS = require('gulp-clean-css');
+var htmlreplace = require('gulp-html-replace');
+var amphtmlValidator = require('amphtml-validator');
+var minifyHTML = require('gulp-minify-html-2');
+var fs = require('fs');
+
+const BUILD_PATH = './build';
+const BUILD_HTML = BUILD_PATH + '/index.html';
+const SOURCE = {
+  'BOOTSTRAP_CSS': './html/css/bootstrap.min.css',
+  'AMPHTML': './amphtml/index.amp.html',
+  'CLEANED_CSS': './amphtml/css/bootstrap.min.css'
+}
+
+// purify removes unused CSS classes
+gulp.task('purify', function() {
+  return gulp.src(SOURCE.BOOTSTRAP_CSS)
+    .pipe(purify([SOURCE.AMPHTML]))
+    .pipe(gulp.dest('./amphtml/css'));
+});
+
+// inline-css inserts the cleaned + minified CSS into HTML
+gulp.task('inline-css', ['purify'], function() {
+  return gulp.src(SOURCE.AMPHTML)
+    .pipe(htmlreplace({
+      'cssInline': {
+        'src': gulp.src(SOURCE.CLEANED_CSS).pipe(cleanCSS()),
+        'tpl': '<style amp-custom>%s</style>'
+      }
+    }))
+    .pipe(gulp.dest(BUILD_PATH));
+});
+
+// validate ensures the AMP HTML is valid
+gulp.task('validate', function() {
+  amphtmlValidator.getInstance().then(function (validator) {
+    var input = fs.readFileSync(BUILD_HTML, 'utf8');
+    var result = validator.validateString(input);
+    ((result.status === 'PASS') ? console.log : console.error)(BUILD_HTML + ": " + result.status);
+    for (var ii = 0; ii < result.errors.length; ii++) {
+      var error = result.errors[ii];
+      var msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+      if (error.specUrl !== null) {
+        msg += ' (see ' + error.specUrl + ')';
+      }
+      ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
+    }
+  });
+});
+
+ 
+gulp.task('minify-html', function() {
+    var opts = {comments:true,spare:true};
+    
+  gulp.src('./build/index.amp.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./minified/'))
+});
+
+// Build task cleans the CSS and inlines it
+gulp.task('build', ['inline-css']);
+// Default task will only validate the build output
+gulp.task('default', ['validate']);
